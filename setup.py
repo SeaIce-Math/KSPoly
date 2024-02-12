b5d8cf8aa8dbe10aa977cd9f89a03ec3ecd7d5e0
mjlatifi <mj.latifi@yahoo.com>
2024-02-12 17:36:59 -0500

    updated setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the contents of the requirements.txt file
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='KSPoly',
     version='1.0',
     packages=find_packages(),
+    install_requires=requirements,
 )
+
+
